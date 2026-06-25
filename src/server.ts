a4428c0fa182049a3e8c80c6dc972b69ecd6185c
Fawwazxdam <fawwazadam1005@gmail,om>
2026-06-25 20:57:19 +0700

    feat: configure CORS middleware with allowed origins and credentials in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import "dotenv/config";
 import app from "./app";
+import cors from "cors";
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+app.use(cors({
+  origin: [
+    'https://memangkas.magentaa.space',
+    'http://localhost:3000'
+  ],
+  credentials: true
+}));
